e858e7e58eb0411f529c8bd9761921cdb6bf6689
site2appsengine-web <site2apps.engine@gmail.com>
2026-07-26 13:15:52 +0500

    Deploy profile PRO-008

---
+++ PRO-008/js/main.js
@@ -0,0 +1,48 @@
+(function ($) {
+    "use strict";
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#nav').addClass('nav-sticky');
+        } else {
+            $('#nav').removeClass('nav-sticky');
+        }
+    });
+    if ($(window).scrollTop() > 100) {
+        $('#nav').addClass('nav-sticky');
+    }
+    
+    
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 50
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+  
+})(jQuery);
+
